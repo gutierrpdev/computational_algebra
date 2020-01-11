34f4c30aaa104372f6584927db8fe00ab2dad8fe
gutierrpdev <gutierrp.dev@gmail.com>
2020-01-11 12:51:18 +0100

    Added support for single term division for Multivariable Polynomials Over Field

--- generic/MultivariablePolynomialsOverField.py
+++ generic/MultivariablePolynomialsOverField.py
@@ -21,6 +21,23 @@ class MultivariablePolynomialsOverField:
                 res[elem] = p2.coefs[elem]
         return MultivariablePolynomial(res, self.field)
 
+    # takes two Multivariable Polynomials with only one term each and returns the division of the two, if possible.
+    # otherwise, returns None.
+    def div_term(self, p1, p2):
+        # only allow division of single terms
+        if len(p1.coefs) != len(p2.coefs) or len(p1.coefs) != 1:
+            return None
+        p1_item = list(p1.coefs.items())[0]
+        p2_item = list(p2.coefs.items())[0]
+        res = [0] * len(p1_item[0])
+
+        for i in range(len(p1_item[0])):
+            if p1_item[0][i] % p2_item[0][i] == 0:
+                res[i] = p1_item[0][i] // p2_item[0][i]
+            else:
+                return None
+        return MultivariablePolynomial({tuple(res): self.field.div(p1_item[1], p2_item[1])}, self.field)
+
 
 if __name__ == "__main__":
     from specific.ZZ import ZZ
@@ -31,3 +48,11 @@ if __name__ == "__main__":
     print("p1 = ", p1)
     print("p2 = ", p2)
     print("p1 + p2 = ", MultivariablePolynomialsOverField(ZZ()).add(p1, p2))
+
+    dic3 = {(2, 4, 6, 8): 25}
+    p3 = MultivariablePolynomial(dic3, ZZ())
+    dic4 = {(2, 2, 3, 2): 5}
+    p4 = MultivariablePolynomial(dic4, ZZ())
+    print("p3 = ", p3)
+    print("p4 = ", p4)
+    print("p3 / p4 = ", MultivariablePolynomialsOverField(ZZ()).div_term(p3, p4))
